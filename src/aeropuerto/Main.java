bee31598b1ec243b9d028b9ab9ede141c2229e9a
Toni Escamilla Pardo <toespar@gmail.com>
2015-05-22 13:00:25 +0200

    Add Main.fxml and controller

--- src/aeropuerto/Main.java
+++ src/aeropuerto/Main.java
@@ -5,11 +5,17 @@
  */
 package aeropuerto;
 
+import aeropuerto.controller.Controlador;
+import application.data.Data;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,26 +27,27 @@ public class Main extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Controlador c = abrirVentana("Main", "Gestión aeropuerto");
+            Data data = Data.getInstance();
     }
-
+    
+    public Controlador abrirVentana(String viewName, String title){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/aeropuerto/view/"+viewName+".fxml"));
+        Parent root = null;
+        Stage stage = new Stage();
+        try {
+            root = (Parent) loader.load();
+        } catch (IOException ex) {}
+        Controlador controller = loader.getController();
+        controller.app=this;
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
+        controller.stage=stage;
+        return controller;
+    }
+    
     /**
      * @param args the command line arguments
      */
---
+++ src/aeropuerto/controller/Controlador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aeropuerto.controller;
+
+import aeropuerto.Main;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author toespar
+ */
+public class Controlador {
+    public Main app;
+    public Stage stage;
+}
