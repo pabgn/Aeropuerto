e0ea23c6e45a2bf7602043198cf174ff6629107c
Pablo García-Nieto Rodríguez <pablo@garcianieto.me>
2015-05-23 10:38:20 +0200

    Gráficas actualizadas para la vista Aeropuertos con filtrado por día.

--- src/aeropuerto/controller/Aeropuerto.java
+++ src/aeropuerto/controller/Aeropuerto.java
@@ -3,6 +3,9 @@ package aeropuerto.controller;
 import application.model.Airport;
 import application.model.AirportFlights;
 import application.model.Flight;
+import application.model.Flight.FlightDomain;
+import application.model.Flight.FlightType;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,10 +13,13 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -53,35 +59,80 @@ public class Aeropuerto extends Controlador {
     private BarChart vuelosPorCompañia;
     @FXML
     private CategoryAxis vuelosPorCompañiaCategorias;
+    @FXML
+    private CategoryAxis vuelosPorHoraCategorias;
+    @FXML
+    private LineChart vuelosPorHora;
+    @FXML
+    private DatePicker date;
     
     List<String> compañias_s;
     public void ready(){
+     //Lista de horas para la gráfica
+        List<String> horas = new ArrayList<>();
+        for(int h=0;h<=23;h++){
+            horas.add(h+"h");
+        }
+        ObservableList<String> horas_o = FXCollections.observableArrayList(horas);
+
      //Cargamos los vuelos
         
-        //Salidas
-     List<Flight> flights = new ArrayList<>(this.app.data.getAirportFlights(aeropuerto).getDepartures().getFlights().values());
+     //Salidas
+     try{
+     List<Flight> flights = new ArrayList<>(this.app.data.getAirportFlights(aeropuerto, date.getValue()).getDepartures().getFlights().values());
      ObservableList<Flight> flights_o = (ObservableList<Flight>)FXCollections.observableArrayList(flights);  
-     tablaVuelosSalidas.setItems(flights_o);
-     
-     //Llegadas
-     List<Flight> flights_l = new ArrayList<>(this.app.data.getAirportFlights(aeropuerto).getArrivals().getFlights().values());
-     ObservableList<Flight> flights_o_l = (ObservableList<Flight>)FXCollections.observableArrayList(flights_l);  
-     tablaVuelosLlegadas.setItems(flights_o_l); 
-     
-     //Obtenemos la lista de compañías de salidas y el numero de vuelos asociados
+        tablaVuelosSalidas.setItems(flights_o);
+        //Obtenemos la lista de compañías de salidas y el numero de vuelos asociados
+     int max=3;
      compañias_s = new ArrayList<>();
-     int[] flightsCounter = new int[12];
+     int[] flightsCounter = new int[max];
+     int[] flightsCounter2 = new int[horas.size()]; //Contador para el numero de vuelos por hora nacionales
+     int[] flightsCounter3 = new int[horas.size()]; //Contador para el numero de vuelos por hora internacionales
+     
      for(Flight f:flights){
+         if(compañias_s.size()<max){
          if(!compañias_s.contains(f.getCompany())){
              compañias_s.add(f.getCompany());
          }
-         flightsCounter[compañias_s.indexOf(f.getCompany())]++;
+         }
+         try{ flightsCounter[compañias_s.indexOf(f.getCompany())]++;} catch(Exception e){}
+         if(f.getFlightDomain()==FlightDomain.INTERNATIONAL){flightsCounter2[f.getDateTime().getHour()]++;}
+         if(f.getFlightDomain()==FlightDomain.NATIONAL){flightsCounter3[f.getDateTime().getHour()]++;}
+
      }
      ObservableList<String> compañias_s_o = FXCollections.observableArrayList(compañias_s);
      vuelosPorCompañiaCategorias.setCategories(compañias_s_o);
      XYChart.Series<String, Integer> series = createDataSeries(flightsCounter, compañias_s);
+     if(vuelosPorCompañia.getData().size()>0){
+        vuelosPorCompañia.getData().remove(0);
+     }
      vuelosPorCompañia.getData().add(series);
      vuelosPorCompañia.setLegendVisible(false);
+     
+     //Vuelos por horas
+     vuelosPorHoraCategorias.setCategories(horas_o);
+     XYChart.Series<String, Integer> series2 = createDataSeries(flightsCounter2, horas);
+     XYChart.Series<String, Integer> series3 = createDataSeries(flightsCounter3, horas);
+
+     vuelosPorHora.getData().add(series2);
+     vuelosPorHora.getData().add(series3);
+     
+     }catch(Exception e){
+        tablaVuelosSalidas.setItems(null);
+        
+        
+     }
+     //Llegadas
+     try{
+     List<Flight> flights_l = new ArrayList<>(this.app.data.getAirportFlights(aeropuerto, date.getValue()).getArrivals().getFlights().values());
+     ObservableList<Flight> flights_o_l = (ObservableList<Flight>)FXCollections.observableArrayList(flights_l);  
+     tablaVuelosLlegadas.setItems(flights_o_l); 
+     }catch(Exception e){
+         tablaVuelosLlegadas.setItems(null);
+     }
+     
+     
+     
     }
     
     
@@ -89,15 +140,22 @@ public class Aeropuerto extends Controlador {
         XYChart.Series<String,Integer> series = new XYChart.Series<String,Integer>();
         for (int i = 0; i < counter.length; i++) {
             if(i<=y.size()-1){
-                XYChart.Data<String, Integer> monthData = new XYChart.Data<String,Integer>(y.get(i), counter[i]);
-                series.getData().add(monthData);
+                XYChart.Data<String, Integer> data = new XYChart.Data<String,Integer>(y.get(i), counter[i]);
+                series.getData().add(data);
             }
         }
 
         return series;
     }
     @FXML
+    private void onDateChanged(Event e){
+        ready();
+    }
+    @FXML
     private void initialize() {
+         //date.setValue(LocalDate.now());
+        date.setValue(LocalDate.of(2015, 3, 9));
+
         //Configuramos cómo se renderizan las tablas y los gráficos
          s_nvuelo.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getFlightNumber()));
          s_destino.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDestiny()));
